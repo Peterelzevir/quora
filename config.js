38403529c2f2247c7205c42e648ea259e6febcb5
Peterelzevir <114345532+Peterelzevir@users.noreply.github.com>
2025-03-30 17:41:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,12 +2,12 @@
 module.exports = {
   TELEGRAM_BOT_TOKEN: '7776609805:AAHnDN-jnhl-TkG0g6FR8b3LnB9B0GeSyNc', // Replace with your actual bot token
   MONGODB_URI: 'mongodb+srv://pinemark02:rHnXK2VsHt6gtBz2@quora.bxfikaw.mongodb.net/?retryWrites=true&w=majority&appName=quora', // Replace with your MongoDB Atlas connection string
-  API_KEY: 'YOUR_API_KEY', // Replace with your API key
-  SECRET_KEY: 'YOUR_SECRET_KEY', // Replace with your secret key
+  API_KEY: '72crqbuf4xi8enlp82agfdb5yhtx17c9', // Replace with your API key
+  SECRET_KEY: '8xhtk56eszmupinl01ymolv5uc269fqb214pkcdogytvjsa77z', // Replace with your secret key
   ADMIN_USERNAME: 'hiyaok',
   SERVICE_ID_1: '24044', // First service ID
-  SERVICE_ID_2: '24047', // Second service ID
+  SERVICE_ID_2: '24046', // Second service ID
   QUANTITY_1: 1000, // Quantity for first service
-  QUANTITY_2: 100, // Quantity for second service
+  QUANTITY_2: 250, // Quantity for second service
   API_URL: 'https://buzzerpanel.id/api/json.php'
 };
