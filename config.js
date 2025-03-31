a5a8dd35f97e72552e307efb7028fd529df2cd4d
Peterelzevir <114345532+Peterelzevir@users.noreply.github.com>
2025-03-31 12:57:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 module.exports = {
   TELEGRAM_BOT_TOKEN: '7776609805:AAHnDN-jnhl-TkG0g6FR8b3LnB9B0GeSyNc', // Replace with your actual bot token
   MONGODB_URI: 'mongodb+srv://pinemark02:rHnXK2VsHt6gtBz2@quora.bxfikaw.mongodb.net/?retryWrites=true&w=majority&appName=quora', // Replace with your MongoDB Atlas connection string
-  API_KEY: '72crqbuf4xi8enlp82agfdb5yhtx17c9', // Replace with your API key
-  SECRET_KEY: 'n74pdp63j071juiqxye52lkiw61rcc29rzgxsbmkasudo4l89o', // Replace with your secret key
+  API_KEY: 'wZCu+FGpa94TjkIY8o7BWA==', // Replace with your API key
+  SECRET_KEY: 'k8923zjfaobmq7cemglsr6dq84iy0s1zbc2553pj6aeovrxh4p', // Replace with your secret key
   ADMIN_USERNAME: 'hiyaok',
   SERVICE_ID_1: '24044', // First service ID
   SERVICE_ID_2: '24046', // Second service ID
